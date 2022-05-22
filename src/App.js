0738f81efd3ed50b8918c88862c5cf0edb6c1802
MinJunKim21 <jinju8012@gmail.com>
2022-05-22 09:38:11 +0900

    doing it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './component/Header';
+import DayList from './component/DayList';
+import Day from './component/Day';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DayList />
+      <Day />
     </div>
   );
 }
